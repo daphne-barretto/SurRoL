45c0cc616d19a7f55c83ac192601032f8ddb268c
Daphne Barretto <dgbscience@gmail.com>
2025-03-01 08:18:24 +1100

    bash files for ddpg, her, herdemo for needlepick, needlereach

--- run/needlereach/ddpg_needlereach.sh
+++ run/needlereach/ddpg_needlereach.sh
@@ -1,5 +1,6 @@
 python -m baselines.run \
 --num_env=2 --alg=ddpg --env=NeedleReach-v0 \
 --num_timesteps=1e5 \
---save_video_interval=1 \
---log_path=../logs/ddpg/NeedleReach-1e5_0
+--save_video_interval=30 \
+--save_path=./policies/ddpg/NeedleReach-1e5_0 \
+--log_path=./logs/ddpg/NeedleReach-1e5_0
