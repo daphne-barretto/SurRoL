155d334aa3fbed5f82a4256a4afb536e8519adac
Daphne Barretto <dbarretto@microsoft.com>
2025-03-10 18:33:49 -0700

    in data_generation.py, add makedirs for demo folder

--- surrol/data/data_generation.py
+++ surrol/data/data_generation.py
@@ -56,6 +56,9 @@ def main():
     folder = 'demo' if not args.video else 'video'
     folder = os.path.join(ROOT_DIR_PATH, 'data', folder)
 
+    # Create the directory if it doesn't exist
+    os.makedirs(folder, exist_ok=True)
+
     np.savez_compressed(os.path.join(folder, file_name),
                         acs=actions, obs=observations, info=infos)  # save the file
 
