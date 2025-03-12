c32f9d73e49fd2e1c4894c89abe9914b60332823
megan <meganliu@stanford.edu>
2025-03-12 03:43:53 -0700

    fix

--- run/gauzeretrieve/hercl_gauzeretrieve_smarterVIDS.sh
+++ run/gauzeretrieve/hercl_gauzeretrieve_smarterVIDS.sh
@@ -2,7 +2,7 @@ python -m baselines.run \
 --num_env=2 --alg=her --env=GauzeRetrieveCurriculumLearningSmarter-v0 \
 --eval_env=GauzeRetrieve-v0 \
 --num_timesteps=1e5 \
---save_path=./policies/hercl/GauzeRetrieveCurriculumLearning-1e5_0 \
---log_path=./logs/hercl/GauzeRetrieveCurriculumLearning-1e5_0 \
+--save_path=./policies/hercl/GauzeRetrieveCurriculumLearningSmarter-1e5_0 \
+--log_path=./logs/hercl/GauzeRetrieveCurriculumLearningSmarter-1e5_0 \
 --n_cycles=20 \
 --save_video_interval=1
