e22334f1b7111d1e6076c67d89221fabb1819c30
megan <meganliu@stanford.edu>
2025-03-12 03:41:59 -0700

    fix

--- run/gauzeretrieve/hercl_gauzeretrieve_smarter.sh
+++ run/gauzeretrieve/hercl_gauzeretrieve_smarter.sh
@@ -2,5 +2,6 @@ python -m baselines.run \
 --num_env=2 --alg=her --env=GauzeRetrieveCurriculumLearningSmarter-v0 \
 --eval_env=GauzeRetrieve-v0 \
 --num_timesteps=1e5 \
---log_path=./logs/hercl/GauzeRetrieveCurriculumLearningSmarter-1e5_0\
---n_cycles=20 \
+--save_path=./policies/hercl/GauzeRetrieveCurriculumLearningSmarter-1e5_0 \
+--log_path=./logs/hercl/GauzeRetrieveCurriculumLearningSmarter-1e5_0 \
+--n_cycles=20 
