e5bdd479d488782598f047ec9a357088f175e0d4
megan <meganliu@stanford.edu>
2025-03-17 22:35:29 -0700

    rename, add print statements

--- surrol/gym/__init__.py
+++ surrol/gym/__init__.py
@@ -54,7 +54,7 @@ register(
 #PSM CL Smarters
 register(
     id='GauzeRetrieveCurriculumLearningSmarter-v0',
-    entry_point='surrol.tasks.gauze_retrieve_curriculum_learning_smarter:GauzeRetrieveCurriculumLearningSmarter',
+    entry_point='surrol.tasks.gauze_retrieve_curriculum_learning_goalmove:GauzeRetrieveCurriculumLearningSmarter',
     max_episode_steps=50,
 )
 
