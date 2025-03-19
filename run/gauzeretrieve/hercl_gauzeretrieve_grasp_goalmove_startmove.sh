2da02e72ef074a96bb2353377e96ea96a9de2c04
megan <meganliu@stanford.edu>
2025-03-18 22:56:58 -0700

    fix startmove bash

--- run/gauzeretrieve/hercl_gauzeretrieve_grasp_goalmove_startmove.sh
+++ run/gauzeretrieve/hercl_gauzeretrieve_grasp_goalmove_startmove.sh
@@ -1,6 +1,6 @@
 python -m baselines.run \
---num_env=2 --alg=her --env=GauzeRetrieveCurriculumLearningGraspGoalMove-v0 \
+--num_env=2 --alg=her --env=GauzeRetrieveCurriculumLearningGraspGoalMoveStartMove-v0 \
 --eval_env=GauzeRetrieve-v0 \
 --num_timesteps=1e5 \
---log_path=./logs/hercl/GauzeRetrieveCurriculumLearningGraspGoalMove-1e5_0 \
+--log_path=./logs/hercl/GauzeRetrieveCurriculumLearningGraspGoalMoveStartMove-1e5_0 \
 --n_cycles=20 
