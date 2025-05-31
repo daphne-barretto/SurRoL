7a406796be69edeecbbe94109d10aaf097ca4508
Daphne Barretto <dbarretto@microsoft.com>
2025-05-31 16:34:12 -0700

    peg transfer variations for data generation

--- surrol/data/data_generation.sh
+++ surrol/data/data_generation.sh
@@ -1,4 +1,4 @@
-# python surrol/data/data_generation.py --env PegTransfer-v0 --num_itr 10 --video &
+python surrol/data/data_generation.py --env PegTransfer-v0 --num_itr 10 --video &
 python surrol/data/data_generation.py --env PegTransfer-v0 --num_itr 100 &
 python surrol/data/data_generation.py --env PegTransfer-v0 --num_itr 1000 &
 python surrol/data/data_generation.py --env PegTransfer-v0 --num_itr 10000
