850b479b3ad0839d91796078169046b44bf4685c
Alycia Lee <alycialee15@yahoo.com>
2025-06-02 05:49:00 +0000

    add two blocks no color shell scripts and task files

---
+++ run/herdemo_pegtransfer_two_blocks_nocolor_onehottargetpeg.sh
@@ -0,0 +1,6 @@
+python -m baselines.run \
+--num_env=2 --alg=her --env=PegTransferTwoBlocksNoColorOneHotTargetPeg-v0 --num_timesteps=2e5 --policy_save_interval=5 \
+--demo_file=../surrol/data/two_blocks/data_PegTransfer-v0_random_1000_2025-06-01_10-17-43_onehottargetpeg.npz \
+--save_path=../policies/her/PegTransferTwoBlocksNoColor-onehottargetpeg-1000demo-2e5_0_r2 \
+--bc_loss=1 --q_filter=1 --num_demo=1000 --demo_batch_size=128 --prm_loss_weight=0.001 --aux_loss_weight=0.0078 --n_cycles=20 --batch_size=1024 --random_eps=0.1 --noise_eps=0.1 \
+--log_path=../logs/her/PegTransferTwoBlocksNoColor-onehottargetpeg-1000demo-2e5_0_r2
---
+++ run/herdemo_pegtransfer_two_blocks_nocolor_targetblock.sh
@@ -0,0 +1,8 @@
+python -m baselines.run \
+--num_env=2 --alg=her --env=PegTransferTwoBlocksNoColorTargetBlock-v0 --num_timesteps=2e5 --policy_save_interval=5 \
+--demo_file=../surrol/data/two_blocks/data_PegTransfer-v0_random_1000_2025-06-01_10-17-43_targetblock.npz \
+--save_path=../policies/her/PegTransferTwoBlocksNoColor-targetblock-1000demo-2e5_0_r1 \
+--bc_loss=1 --q_filter=1 --num_demo=1000 --demo_batch_size=128 --prm_loss_weight=0.001 --aux_loss_weight=0.0078 --n_cycles=20 --batch_size=1024 --random_eps=0.1 --noise_eps=0.1 \
+--log_path=../logs/her/PegTransferTwoBlocksNoColor-targetblock-1000demo-2e5_0_r1
+
+## save and log path suffix r0 = run #0, r1 = run #1, r2 = run #2
