e8674b5e69112ce9e2eb8c658cfce7f9bc2cc996
Daphne Barretto <dgbscience@gmail.com>
2025-06-03 09:08:30 +0000

    reorganize init.py with environment categories and clean up unused environments

--- surrol/gym/__init__.py
+++ surrol/gym/__init__.py
@@ -1,7 +1,8 @@
 from gym.envs.registration import register
 
 
-# PSM Env
+##### SurRoL Patient-Side Manipulator Environments #####
+
 register(
     id='NeedleReach-v0',
     entry_point='surrol.tasks.needle_reach:NeedleReach',
@@ -26,6 +27,64 @@ register(
     max_episode_steps=50,
 )
 
+##### Goal-Conditioned Peg Transfer Environments #####
+
+# Baselines: Target Block Position
+
+register(
+    id='PegTransferTBOTwoBlock-v0',
+    entry_point='surrol.tasks.peg_transfer-two_blocks-with_only_target_block:PegTransfer',
+    max_episode_steps=50,
+)
+
+register(
+    id='PegTransferTBO-v0',
+    entry_point='surrol.tasks.peg_transfer_with_only_target_block:PegTransfer',
+    max_episode_steps=50,
+)
+
+# 2 Blocks with All block positions
+
+register(
+    id='PegTransferTwoBlocksAllBlocks-v0',
+    entry_point='surrol.tasks.peg_transfer-two_blocks-with_all_blocks-no_obs:PegTransfer',
+    max_episode_steps=50,
+)
+
+register(
+    id='PegTransferTwoBlocksTargetBlock-v0',
+    entry_point='surrol.tasks.peg_transfer-two_blocks-with_all_blocks-no_obs_target_block:PegTransfer',
+    max_episode_steps=50,
+)
+
+# 2 Blocks with All block positions and colors
+
+register(
+    id='PegTransferTwoBlocksOneHot-v0',
+    entry_point='surrol.tasks.peg_transfer-two_blocks-with_all_blocks_colored-no_obs:PegTransfer',
+    max_episode_steps=50,
+)
+
+register(
+    id='PegTransferTwoBlocksColored-v0',
+    entry_point='surrol.tasks.peg_transfer-two_blocks-with_all_blocks_colored-no_obs:PegTransfer',
+    max_episode_steps=50,
+)
+
+register(
+    id='PegTransferTwoBlocksFourTuple-v0',
+    entry_point='surrol.tasks.peg_transfer-two_blocks-with_all_blocks_colored-no_obs_four_tuple:PegTransfer',
+    max_episode_steps=50,
+)
+
+register(
+    id='PegTransferTwoBlocksColorTargetBlock-v0',
+    entry_point='surrol.tasks.peg_transfer-two_blocks-with_all_blocks_colored-targetblock:PegTransfer',
+    max_episode_steps=50,
+)
+
+# 4 Blocks with All block positions
+
 register(
     id='PegTransferTargetBlock-v0',
     entry_point='surrol.tasks.peg_transfer_target_block:PegTransfer',
@@ -39,16 +98,18 @@ register(
 )
 
 register(
+    id='PegTransferAllBlocksFourTuple-v0',
+    entry_point='surrol.tasks.peg_transfer_four_tuple:PegTransfer',
+    max_episode_steps=50,
+)
+
+register(
     id='PegTransferOneHotTargetPeg-v0',
     entry_point='surrol.tasks.peg_transfer_onehot_and_target_peg:PegTransfer',
     max_episode_steps=50,
 )
 
-register(
-    id='PegTransferAllBlocksFourTuple-v0',
-    entry_point='surrol.tasks.peg_transfer_four_tuple:PegTransfer',
-    max_episode_steps=50,
-)
+# 4 Blocks with All block positions and colors
 
 register(
     id='PegTransferColor-v0',
@@ -75,18 +136,6 @@ register(
 )
 
 register(
-    id='PegTransferTBO-v0',
-    entry_point='surrol.tasks.peg_transfer_with_only_target_block:PegTransfer',
-    max_episode_steps=50,
-)
-
-register(
-    id='PegTransferTBOTwoBlock-v0',
-    entry_point='surrol.tasks.peg_transfer-two_blocks-with_only_target_block:PegTransfer',
-    max_episode_steps=50,
-)
-
-register(
     id='PegTransferFourTuple-v0',
     entry_point='surrol.tasks.peg_transfer_with_all_blocks_colored_four_tuple:PegTransfer',
     max_episode_steps=50,
@@ -95,117 +144,5 @@ register(
 register(
     id='PegTransferColorLanguage-v0',
     entry_point='surrol.tasks.peg_transfer_with_all_blocks_colored_color_language:PegTransfer',
-    max_episode_steps=50,
-)
-
-register(
-    id='PegTransferTwoBlocksOneHot-v0',
-    entry_point='surrol.tasks.peg_transfer-two_blocks-with_all_blocks_colored-no_obs:PegTransfer',
-    max_episode_steps=50,
-)
-
-register(
-    id='PegTransferTwoBlocksColored-v0',
-    entry_point='surrol.tasks.peg_transfer-two_blocks-with_all_blocks_colored-no_obs:PegTransfer',
-    max_episode_steps=50,
-)
-
-register(
-    id='PegTransferTwoBlocksAllBlocks-v0',
-    entry_point='surrol.tasks.peg_transfer-two_blocks-with_all_blocks-no_obs:PegTransfer',
-    max_episode_steps=50,
-)
-
-register(
-    id='PegTransferTwoBlocksFourTuple-v0',
-    entry_point='surrol.tasks.peg_transfer-two_blocks-with_all_blocks_colored-no_obs_four_tuple:PegTransfer',
-    max_episode_steps=50,
-)
-
-register(
-    id='PegTransferTwoBlocksTargetBlock-v0',
-    entry_point='surrol.tasks.peg_transfer-two_blocks-with_all_blocks-no_obs_target_block:PegTransfer',
-    max_episode_steps=50,
-)
-
-register(
-    id='PegTransferTwoBlocksColorTargetBlock-v0',
-    entry_point='surrol.tasks.peg_transfer-two_blocks-with_all_blocks_colored-targetblock:PegTransfer',
-    max_episode_steps=50,
-)
-
-register(
-    id='NeedleReachRandom-v0',
-    entry_point='surrol.tasks.needle_reach_random:NeedleReachRandom',
-    max_episode_steps=50,
-)
-
-# PSM Curriculum Learning Env
-register(
-    id='NeedleReachCurriculumLearning-v0',
-    entry_point='surrol.tasks.needle_reach_curriculum_learning:NeedleReachCurriculumLearning',
-    max_episode_steps=50,
-)
-
-register(
-    id='GauzeRetrieveCurriculumLearning-v0',
-    entry_point='surrol.tasks.gauze_retrieve_curriculum_learning:GauzeRetrieveCurriculumLearning',
-    max_episode_steps=50,
-)
-
-register(
-    id='NeedlePickCurriculumLearning-v0',
-    entry_point='surrol.tasks.needle_pick_curriculum_learning:NeedlePickCurriculumLearning',
-    max_episode_steps=50,
-)
-
-register(
-    id='PegTransferCurriculumLearning-v0',
-    entry_point='surrol.tasks.peg_transfer_curriculum_learning:PegTransferCurriculumLearning',
-    max_episode_steps=50,
-)
-
-#PSM CL Smarters
-register(
-    id='GauzeRetrieveCurriculumLearningSmarter-v0',
-    entry_point='surrol.tasks.gauze_retrieve_curriculum_learning_goalmove:GauzeRetrieveCurriculumLearningSmarter',
-    max_episode_steps=50,
-)
-
-register(
-    id='GauzeRetrieveCurriculumLearningGraspGoalMove-v0',
-    entry_point='surrol.tasks.gauze_retrieve_curriculum_learning_grasp_goalmove:GauzeRetrieveCurriculumLearningGraspGoalMove',
-    max_episode_steps=50,
-)
-
-register(
-    id='GauzeRetrieveCurriculumLearningGraspGoalMoveStartMove-v0',
-    entry_point='surrol.tasks.gauze_retrieve_curriculum_learning_grasp_goalmove_startmove:GauzeRetrieveCurriculumLearningGraspGoalMoveStartMove',
-    max_episode_steps=50,
-)
-
-# PSM Contact Approx Tasks
-register(
-    id='GauzeRetrieveContactApprox-v0',
-    entry_point='surrol.tasks.gauze_retrieve_contact_approx:GauzeRetrieveContactApprox',
-    max_episode_steps=50,
-)
-
-register(
-    id='GauzeRetrieveContactApproxCurriculumLearning-v0',
-    entry_point='surrol.tasks.gauze_retrieve_contact_approx_curriculum_learning:GauzeRetrieveContactApproxCurriculumLearning',
-    max_episode_steps=50,
-)
-
-# PSM Auto Grasp Tasks
-register(
-    id='GauzeRetrieveAutoGrasp-v0',
-    entry_point='surrol.tasks.gauze_retrieve_auto_grasp:GauzeRetrieveAutoGrasp',
-    max_episode_steps=50,
-)
-
-register(
-    id='GauzeRetrieveAutoGraspCurriculumLearning-v0',
-    entry_point='surrol.tasks.gauze_retrieve_auto_grasp_curriculum_learning:GauzeRetrieveAutoGraspCurriculumLearning',
     max_episode_steps=50,
 )
